9cc0a6bf61ed0a433954f2aba86dc04f2e09a4cd
Zhuldyz Akhmetova <zakhmetova1@my.bcit.ca>
2026-02-02 12:36:59 -0800

    SASS setup and included tailwind-sass-intagration documents, scss files icluded in styles

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,html}",
+    "./public/**/*.html",
+    "./styles/**/*.css"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
